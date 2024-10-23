6edacd3d39fc00eefb2624f09abd957f668c9cc9
Nachete99 <nacho.ferrando99@gmail.com>
2024-10-23 15:45:12 +0200

    RK4 Fixed in Milestone 1

--- Milestone_1/Milestone_1.py
+++ Milestone_1/Milestone_1.py
@@ -68,11 +68,11 @@ def Runge_Kutta_4(xn, yn, xdn, ydn, dt, n_lim):
         K3_dx = -(x[n] + 0.5*dt*K2_x)/r3**(3/2)
         K3_dy = -(y[n] + 0.5*dt*K2_y)/r3**(3/2)
 
-        K4_x = dx[n] + 0.5*dt*K3_dx
-        K4_y = dy[n] + 0.5*dt*K3_dy
-        r4 = ((x[n] + 0.5*dt*K3_x)**2 + (y[n] + 0.5*dt*K3_y)**2)
-        K4_dx = -(x[n] + 0.5*dt*K3_x)/r4**(3/2)
-        K4_dy = -(y[n] + 0.5*dt*K3_y)/r4**(3/2)
+        K4_x = dx[n] + dt*K3_dx
+        K4_y = dy[n] + dt*K3_dy
+        r4 = ((x[n] + dt*K3_x)**2 + (y[n] + dt*K3_y)**2)
+        K4_dx = -(x[n] + dt*K3_x)/r4**(3/2)
+        K4_dy = -(y[n] + dt*K3_y)/r4**(3/2)
 
         x.append(x[n] + dt * (K1_x + 2*K2_x + 2*K3_x + K4_x)/6 )
         y.append(y[n] + dt * (K1_y + 2*K2_y + 2*K3_y + K4_y)/6 )
@@ -84,6 +84,14 @@ def Runge_Kutta_4(xn, yn, xdn, ydn, dt, n_lim):
     plot(x, y, 'RK4')
         
     return
+
+
+def Euler_inverso(U, dt, t, F):
+    def G(X):
+        return  X - U - dt * F(X,t)
+
+    return 
+
 
 def plot(x, y, method):
 
@@ -118,5 +126,6 @@ if __name__ == "__main__":
     Euler(x_0, y_0, dx_0, dy_0, dT, n_lim)
     Crank_Nicolson(x_0, y_0, dx_0, dy_0, dT, n_lim)
     Runge_Kutta_4(x_0, y_0, dx_0, dy_0, dT, n_lim)
+    Euler_inverso()
 
     print('END OF SCRIPT')
